219c03c44d095cfac8b45831e53f90c68317bc3d
Guilherme-Goldmann <guilhermegarguedes@gmail.com>
2025-11-04 12:52:27 -0300

    rm terragrunt-cache dir

--- accounts/ec2-example-1/.terragrunt-cache/UfZX1FHy5n4DsKcFGeHekXwp-6s/v_q0cBSyYTIL5zCz0j2fqULeHuk/main.tf
+++
@@ -1,9 +0,0 @@
-# Main file of the module, defining the resource(s) to be provisioned
-
-resource "aws_instance" "demo-instance" {
-  instance_type = var.instance_type
-  key_name = var.key_name
-  subnet_id = var.subnet_id
-  ami = var.ami
-  tags = var.tags
-}
